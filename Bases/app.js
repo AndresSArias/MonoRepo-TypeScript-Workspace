bda4846a47d2f65ecd08fc6721724c328bd6feb9
AndresSArias <andres31321@gmail.com>
2024-07-25 03:25:37 -0500

    [Init] Repository with the introducction of TypeScript in folder bases

---
+++ Bases/app.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+    nom_varible : Tipo_de_varible
+
+    Para el caso de la siguiente variable constante define al tipo como su contenido
+    const msg: 'Hola mundo'
+
+ */
+const msg = 'Hola mundo';
+/*
+    En este caso al usar let se define así
+    let msg : string
+    Por ende dentro del desarro con esta variable, no se puede asignar algo diferente.
+*/
+let msg2 = 'Hola mundo';
+let msg3 = "Chao mundo";
+console.log(msg);
---
+++ Bases/app.ts
@@ -0,0 +1,19 @@
+/* 
+    nom_varible : Tipo_de_varible
+
+    Para el caso de la siguiente variable constante define al tipo como su contenido
+    const msg: 'Hola mundo'
+
+ */
+const msg:string = 'Hola mundo';
+
+/* 
+    En este caso al usar let se define así
+    let msg : string
+    Por ende dentro del desarro con esta variable, no se puede asignar algo diferente.
+*/
+let msg2:string = 'Hola mundo';
+
+let msg3:string = "Chao mundo"
+
+console.log(msg)
